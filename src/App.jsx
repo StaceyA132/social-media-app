9a8174f6f61e7b7ea1fada90fe291817f0fbcac0
StaceyA132 <staceyannan28@gmail.com>
2024-03-30 07:07:27 -0400

    passed props to post component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import reactLogo from './assets/react.svg';
 import viteLogo from './vite.svg';
 import './App.css';
 import Post from './components/Post';
+import Post from './components/Post';
+
+
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
+  return(
+    
     <div>
-      <h1>Fakebook!</h1>
-      {/* CreatePostForm */}
-      {/* Feed */}
-      {/* Post */}
+    <h1>Fakebook!</h1>
+    {/* CreatePostForm */}
+    {/* Feed */}
+    <Post content="This is a test post!" />
+    <Post content="This is another test post!" />
     </div>
-  )
+    )
 }
 
 export default App;
